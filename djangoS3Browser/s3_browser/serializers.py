cf5e92eb15519afe167c4809ade99f4eb5eed33c
Kalkidan Teklu <kalkidanteklu4@gmail.com>
2022-07-04 17:21:14 +0300

    adds serializers

--- djangoS3Browser/s3_browser/serializers.py
+++ djangoS3Browser/s3_browser/serializers.py
@@ -3,10 +3,30 @@ from rest_framework import serializers
 
 class FileSerializer(serializers.Serializer):
     key = serializers.CharField(max_length=255)
-    url = serializers.FileField()
-    icon = serializers.FileField()
+    url = serializers.URLField()
     text = serializers.CharField(max_length=255)
-    file_type = serializers.CharField(max_length=255)
+    file_type = serializers.CharField(max_length=255, source="type")
+
+class UploadFileSerializer(serializers.Serializer):
+    file = serializers.FileField()
+    loc = serializers.CharField()
 
 class BucketSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255)
+
+class CreateFolderSerializer(serializers.Serializer):
+    loc = serializers.CharField(max_length=255)
+    name = serializers.CharField(max_length=255)
+
+class FileRenameSerializer(serializers.Serializer):
+    loc = serializers.CharField()
+    old_name = serializers.CharField(max_length=255)
+    new_name = serializers.CharField(max_length=255)
+
+
+class FileDeleteSerializer(serializers.Serializer):
+    file_list = serializers.ListField(child=serializers.CharField())
+
+class PasteFileSerializer(serializers.Serializer):
+    loc = serializers.CharField(max_length=255)
+    file_list = serializers.ListField(child=serializers.CharField())
