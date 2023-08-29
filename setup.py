9d31f9d7e1533b48d74646a0c9e3fae06a8e3a86
kalkidan-flawless-workflow <88381268+kalkidan-flawless-workflow@users.noreply.github.com>
2023-08-29 10:10:05 +0300

    removes acl (#2)
    Co-authored-by: Kalkidan Teklu <kalkidanteklu4@gmail.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, "README.md")).read()
 
 setup(
     name="DjangoS3Browser",
-    version="0.2.8",
+    version="0.2.9",
     packages=[
         "djangoS3Browser",
         "djangoS3Browser.s3_browser",
